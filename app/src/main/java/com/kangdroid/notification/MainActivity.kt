4708880bc9918d118e17315d51e597eea8803a56
KangDroid <hyunwoo286@gmail.com>
2021-01-01 21:25:47 +0900

    Check whether notification access is granted

--- app/src/main/java/com/kangdroid/notification/MainActivity.kt
+++ app/src/main/java/com/kangdroid/notification/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.kangdroid.notification
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!isNotificationGranted()) {
+            val intent = Intent( "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        }
         setContentView(R.layout.activity_main)
+    }
+
+    fun isNotificationGranted(): Boolean {
+        val mNotifSets = NotificationManagerCompat.getEnabledListenerPackages(this)
+        return mNotifSets.contains(packageName)
     }
 }
