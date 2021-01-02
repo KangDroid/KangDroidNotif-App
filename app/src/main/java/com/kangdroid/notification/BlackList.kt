8d1989f6b88fd86ff5586663a43db71de0633fe0
KangDroid <hyunwoo286@gmail.com>
2021-01-02 20:32:29 +0900

    Add ProgressDialog when showing installed app lists
    Since ProgressDialog and AsyncTask IS deprecated,
    
    however, google noted progressdialog is non-interacture.
    But in this case, like requesting App list from system, user should not interact with this application, rather user needs to be waited.
    So use ProgressDialog.
    
    TODO: Migrate AsyncTask to Kotlin Coroutine

--- app/src/main/java/com/kangdroid/notification/BlackList.kt
+++ app/src/main/java/com/kangdroid/notification/BlackList.kt
@@ -1,49 +1,76 @@
 package com.kangdroid.notification
 
 import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.SharedPreferences
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.os.Bundle
 import android.content.pm.PackageManager
+import android.os.AsyncTask
+import android.util.Log
 import androidx.preference.*
 import com.kangdroid.notification.settings.Settings
 
 class BlackList: PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
     var mSwitchMap: HashMap<SwitchPreference, String> = HashMap()
+    lateinit var mProgressDialog: ProgressDialog
+    lateinit var mScreen: PreferenceScreen
+    lateinit var mSharedPreference: SharedPreferences
+    lateinit var mThisWrapper: BlackList
+
+    inner class ShowProgress: AsyncTask<Void, Void, Unit>() {
+
+        override fun onPreExecute() {
+            mProgressDialog.show()
+        }
+
+        @SuppressLint("WrongConstant")
+        override fun doInBackground(vararg params: Void?) {
+            val mPackageManager = activity?.packageManager
+            val mPackages: List<PackageInfo> = mPackageManager?.getInstalledPackages(PackageManager.MATCH_ALL)!!
+            for (i in mPackages.indices) {
+                if (mPackages[i].applicationInfo.flags.and(ApplicationInfo.FLAG_SYSTEM) == 1) {
+                    continue
+                }
+
+                var mSwitch = SwitchPreference(activity)
+                with (mSwitch) {
+                    title = mPackages[i].applicationInfo.loadLabel(mPackageManager)
+                    icon = mPackages[i].applicationInfo.loadIcon(mPackageManager)
+                    key = "switch_" + mPackages[i].packageName
+
+                    Settings.mHashBlackList[mPackages[i].packageName] = mSharedPreference.getBoolean(key, false)
+                    setDefaultValue(false)
+                }
+
+                mSwitchMap[mSwitch] = mPackages[i].packageName
+                mSwitch.onPreferenceChangeListener = mThisWrapper
+                mScreen.addPreference(mSwitch)
+            }
+
+            // Set PreferenceScreens
+            preferenceScreen = mScreen
+            return
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            mProgressDialog.dismiss()
+        }
+    }
 
     @SuppressLint("WrongConstant")
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        mProgressDialog = ProgressDialog(activity)
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        mProgressDialog.setMessage("Getting Application Lists...")
+
         // Shared Preference for getting values
-        val mSharedPreference = PreferenceManager.getDefaultSharedPreferences(activity)
+        mSharedPreference = PreferenceManager.getDefaultSharedPreferences(activity)
+        mScreen = preferenceManager.createPreferenceScreen(activity)
+        mThisWrapper = this
 
-        val screen: PreferenceScreen = preferenceManager.createPreferenceScreen(activity)
-
-        // UI Initiate Code starts here.
-
-        val mPackageManager = activity?.packageManager
-        val mPackages: List<PackageInfo> = mPackageManager?.getInstalledPackages(PackageManager.MATCH_ALL)!!
-        for (i in mPackages.indices) {
-            if (mPackages[i].applicationInfo.flags.and(ApplicationInfo.FLAG_SYSTEM) == 1) {
-                continue
-            }
-
-            var mSwitch = SwitchPreference(activity)
-            with (mSwitch) {
-                title = mPackages[i].applicationInfo.loadLabel(mPackageManager)
-                icon = mPackages[i].applicationInfo.loadIcon(mPackageManager)
-                key = "switch_" + mPackages[i].packageName
-
-                Settings.mHashBlackList[mPackages[i].packageName] = mSharedPreference.getBoolean(key, false)
-                setDefaultValue(false)
-            }
-
-            mSwitchMap[mSwitch] = mPackages[i].packageName
-            mSwitch.onPreferenceChangeListener = this
-            screen.addPreference(mSwitch)
-        }
-
-        // Set PreferenceScreens
-        preferenceScreen = screen
+        ShowProgress().execute()
     }
 
     /**
